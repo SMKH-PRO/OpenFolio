1c5499c1f71b2bb0b59519677fb410c90f1dc43e
kashan <smkh.kashan@gmail.com>
2022-06-08 22:19:45 +0500

    tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
   <Component {...pageProps} />
 );
 
